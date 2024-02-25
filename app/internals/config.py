ce17b0daa48570b5369ef58d2fc3cd4aa5d01b30
Jason Walker <desktophero@gmail.com>
2024-02-24 20:35:05 -0600

    feat: add docs, scripts, and setup (#1)
    * feat: readme update
    
    Signed-off-by: Jason Walker <desktophero@gmail.com>
    
    * more readme updates
    
    Signed-off-by: Jason Walker <desktophero@gmail.com>
    
    * add content on prep, setup, and executing
    
    Signed-off-by: Jason Walker <desktophero@gmail.com>
    
    ---------
    
    Signed-off-by: Jason Walker <desktophero@gmail.com>

---
+++ app/internals/config.py
@@ -0,0 +1,6 @@
+PDF_DOCUMENT_PATH = "data/docs/gatsby.pdf"
+CHUNK_SIZE = 250 
+CHUNK_OVERLAP = 20
+CHROMA_PATH = "chroma"
+MODEL_NAME = "mistral-openorca"
+EMBEDDINGS_NAME = "sentence-transformers/all-mpnet-base-v2"
